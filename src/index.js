356f2c90d29030dbd6737f4b6440a55f37e506f5
Sctache <27soc12@gmail.com>
2025-09-07 12:07:49 -0300

    Explicação do Desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log ("Oi!")
+//Criar uma variável para o nome do herói.
+
+//Criar uma variável para o XP (números inteiros).
+
+//Se 'XP <= 1000' ==
+//Se '1001 <= XP <= 2000' ==
+//Se '2001 <= XP <= 5000' ==
+//Se '5001 <= XP <= 7000' ==
+//Se '7001 <= XP <= 8000' ==
+//Se '8001 <= XP <= 9000' ==
+//Se '9001 <= XP <= 10000' ==
