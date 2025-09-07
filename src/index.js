5acfdf0a1065e072f6f1031172d8bb5dae1fca80
Sctache <27soc12@gmail.com>
2025-09-07 12:33:55 -0300

    Utilizando 'if' e 'else'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 let nome = "Sookie"
 
 //Criar uma variável para o XP (números inteiros).
-let nivel = int(10)
+let xp = int(1000)
+let nivel = 
 
 //Se 'XP <= 1000' == Ferro
 //Se '1001 <= XP <= 2000' == Bronze
@@ -12,6 +13,43 @@ let nivel = int(10)
 //Se '8001 <= XP <= 9000' == Ascendente
 //Se '9001 <= XP <= 10000' == Imortal
 //Se 'XP > 10000' == Radiante
+
+if (xp <= 1000){
+    nivel = "Ferro"
+}
+    else{
+        if (1001 <= xp <= 2000){
+            nivel = "Bronze"
+        }
+    }
+        else{
+            if (2001 <= xp <= 5000){
+                nivel = "Prata"
+            }
+        }
+            else{
+               if (5001 <= xp <= 7000){
+                nivel = "Ouro"
+               } 
+            } 
+                else{
+                    if (7001 <= xp <= 8000){
+                        nivel = "Platina"
+                    }
+                }
+                    else{
+                        if (8001 <= xp <= 9000){
+                            nivel = "Ascendente"
+                        }
+                    }
+                        else{
+                            if (9001 <= xp <= 10000){
+                                nivel = "Imortal"
+                            }
+                        }
+                            else{
+                                nivel = "Radiante"
+                            }
 
 //Saída: "O Herói de nome **{nome}** está no nível de **{nivel}**."
 console.log ("O Herói de nome " + nome + "está no nível de " + nivel + ".")
