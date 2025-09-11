0134f53e09c6cc8ba1a6326b4bd7efe6132498de
Sctache <27soc12@gmail.com>
2025-09-11 17:15:08 -0300

    Finalizado!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 //Criar uma variável para o nome do herói.
-let nome = "Sookie"
+let nome = "Felipão"
 
 //Criar uma variável para o XP (números inteiros).
 let xp = 1000
+let nivel = ""
 
 
 //Se 'XP <= 1000' == Ferro
@@ -14,34 +15,39 @@ let xp = 1000
 //Se '9001 <= XP <= 10000' == Imortal
 //Se 'XP > 10000' == Radiante
 
-switch (xp){
-    case 0<=xp<=1000: console.log("Ferro")
+switch (true){
+    case 0<=xp<=1000: 
+    nivel = "Ferro"
     break
 
-    case 1001<=xp<=2000: console.log("Bronze")
+    case 1001<=xp<=2000:
+    nivel = "Bronze"
     break
 
-    case 2001<=xp<=5000: console.log("Prata")
+    case 2001<=xp<=5000:
+    nivel = "Prata" 
     break
 
-    case 5001<=xp<=7000: console.log("Ouro")
+    case 5001<=xp<=7000: 
+    nivel = "Ouro"
     break
 
-    case 7001<=xp<=8000: console.log("Platina")
+    case 7001<=xp<=8000: 
+    nivel = "Platina"
     break
 
-    case 8001<=xp<=9000: console.log("Ascendente")
+    case 8001<=xp<=9000: 
+    nivel = "Ascendente"
     break
 
-    case 9001<=xp<=10000: console.log("Imortal")
+    case 9001<=xp<=10000: 
+    nivel = "Imortal"
     break
 
-    case xp>10000: console.log("Radiante")
+    case xp>10000: 
+    nivel = "Radiante"
     break
-
-    default:
-    console.log("O número digitado é inválido.")
 }
 
 //Saída: "O Herói de nome **{nome}** está no nível de **{nivel}**."
-console.log ("O Herói de nome " + nome + "está no nível de " + xp + ".")
+console.log ("O Herói de nome " + nome + " está no nível de " + nivel + ".")
