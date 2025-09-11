68d64be4cc31a0be34326c7f44f4d0b735f2008a
Sctache <27soc12@gmail.com>
2025-09-11 16:54:04 -0300

    Trocando por Switch-Case

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let nome = "Sookie"
 
 //Criar uma variável para o XP (números inteiros).
 let xp = int(1000)
-let nivel = 
+
 
 //Se 'XP <= 1000' == Ferro
 //Se '1001 <= XP <= 2000' == Bronze
@@ -14,42 +14,34 @@ let nivel =
 //Se '9001 <= XP <= 10000' == Imortal
 //Se 'XP > 10000' == Radiante
 
-if (xp <= 1000){
-    nivel = "Ferro"
+switch (xp){
+    case 0<=xp<=1000: console.log("Ferro")
+    break
+
+    case 1001<=xp<=2000: console.log("Bronze")
+    break
+
+    case 2001<=xp<=5000: console.log("Prata")
+    break
+
+    case 5001<=xp<=7000: console.log("Ouro")
+    break
+
+    case 7001<=xp<=8000: console.log("Platina")
+    break
+
+    case 8001<=xp<=9000: console.log("Ascendente")
+    break
+
+    case 9001<=xp<=10000: console.log("Imortal")
+    break
+
+    case xp>10000: console.log("Radiante")
+    break
+
+    default:
+    console.log("O número digitado é inválido.")
 }
-    else{
-        if (1001 <= xp <= 2000){
-            nivel = "Bronze"
-        }
-    }
-        else{
-            if (2001 <= xp <= 5000){
-                nivel = "Prata"
-            }
-        }
-            else{
-               if (5001 <= xp <= 7000){
-                nivel = "Ouro"
-               } 
-            } 
-                else{
-                    if (7001 <= xp <= 8000){
-                        nivel = "Platina"
-                    }
-                }
-                    else{
-                        if (8001 <= xp <= 9000){
-                            nivel = "Ascendente"
-                        }
-                    }
-                        else{
-                            if (9001 <= xp <= 10000){
-                                nivel = "Imortal"
-                            }
-                        }
-                            else{
-                                nivel = "Radiante"
-                            }
 
 //Saída: "O Herói de nome **{nome}** está no nível de **{nivel}**."
-console.log ("O Herói de nome " + nome + "está no nível de " + nivel + ".")
+console.log ("O Herói de nome " + nome + "está no nível de " + xp + ".")
